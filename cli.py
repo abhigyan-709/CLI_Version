c8c8925fb922709037458147b8a0d22bc350453c
Abhigyan Kumar <abhigkumar709@gmail.com>
2022-12-01 03:06:35 +0530

    Test for Time

--- cli.py
+++ cli.py
@@ -6,6 +6,7 @@ import sys
 def EquipmentDetails():
     global equipment_name
     global instrument_number
+
     validInt = False
     while not validInt:
         equipment_name = input("Please enter the euipment name: ")
@@ -41,7 +42,30 @@ def inputDate():
             print("Wrong input format given, please try again!.")
 
 def inputTime():
-    pass
+    global minutes
+    global fromTime
+    global from_time
+    global toTime
+    global to_time
+    global delta
+    valid_int = False
+
+    while not valid_int:
+        try:
+            fromTime = input("Enter from time (HH:MM:SS): ")
+            toTime = input("Enter to time (HH:MM:SS): ")
+            from_time = datetime.strptime(fromTime, "%H:%M:%S")
+            to_time = datetime.strptime(toTime, "%H:%M:%S")
+            print('Start Time: ', from_time.time())
+            print('End Time: ', to_time.time())
+            delta = abs(from_time - to_time)
+            seconds = delta.total_seconds()
+            minutes = seconds / 60
+            print("Difference between time in minutes: ", minutes)
+            valid_int = True
+        except ValueError:
+            print("Wrong time format given, please try again.")
+
 
 def inputTemp():
     pass
