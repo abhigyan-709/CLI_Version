507968fbe7d3d8b86ea007e3a9d6ff088725b2b9
Abhigyan Kumar <abhigkumar709@gmail.com>
2022-12-04 22:01:49 +0530

    Changes Start for CLI

--- cli.py
+++ cli.py
@@ -26,17 +26,17 @@ def EquipmentDetails():
 def inputDate():
     global from_date
     global to_date
-    global no_days
+    # global no_days
 
     valid_int = False
     while not valid_int:
         try:
-            fromDate = input("Enter from date (DD/MM/YYYY): ")
-            toDate = input("Enter to Date (DD/MM/YYYY): ")
-            from_date = datetime.strptime(fromDate,"%d/%m/%Y").date()
-            to_date = datetime.strptime(toDate,"%d/%m/%Y").date()
-            no_days = abs((from_date-to_date).days)
-            print("Difference between Date: ", no_days)
+            fromDate = input("Enter from date (DD/MM/YYYY HH:MM): ")
+            toDate = input("Enter to Date (DD/MM/YYYY HH:MM): ")
+            from_date = datetime.strptime(fromDate,"%d/%m/%Y %H:%M")
+            to_date = datetime.strptime(toDate, "%d/%m/%Y %H:%M").date()
+            # no_days = abs((from_date-to_date).days)
+            # print("Difference between Date: ", no_days)
             valid_int = True
         except ValueError:
             print("Wrong input format given, please try again!.")
